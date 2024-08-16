2a88364a44c7ba0c32284ee6e00df4eb99f5c8e3
MagicExist <johhanparra1@gmail.com>
2024-08-16 15:08:45 -0500

    Student Service Added

---
+++ SchoolApi.Application/Services/StudentDBService.cs
@@ -0,0 +1,21 @@
+﻿using SchoolApi.Domain.Entities;
+using SchoolApi.Domain.Repository;
+
+namespace SchoolApi.Application.Services
+{
+    public sealed class StudentDBService
+    {
+        private readonly IStudentDBRepository _studentDBRepository;
+        public StudentDBService(IStudentDBRepository studentDBRepository) 
+        {
+            _studentDBRepository = studentDBRepository;
+        }
+
+        public async Task<IEnumerable<Student>> ReadAsync()
+        {
+            IEnumerable<Student> studentsList = await _studentDBRepository.GetAllStudentsAsync();
+            return studentsList;
+        }
+
+    }
+}
