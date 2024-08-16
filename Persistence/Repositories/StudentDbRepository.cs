69ea5dcfd30830e6c810b7d9a9c2557900d6b4cf
MagicExist <johhanparra1@gmail.com>
2024-08-16 15:15:10 -0500

    StudentDBRepository from presentation layer added

---
+++ Persistence/Repositories/StudentDbRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.DataBase;
+using SchoolApi.Domain.Entities;
+using SchoolApi.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    internal class StudentDbRepository : IStudentDBRepository
+    {
+        private readonly SchoolDbContext _dbContext;
+        public StudentDbRepository(SchoolDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<Student>> GetAllStudentsAsync()
+        {
+            return await _dbContext.Students.ToListAsync();
+        }
+    }
+}
