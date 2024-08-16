3b68bbc23d357e9d6e30944e119750ea04c2ee2f
MagicExist <johhanparra1@gmail.com>
2024-08-16 15:35:48 -0500

    StudentController Feature Added

---
+++ SchoolApi/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolApi.Application.Services;
+using SchoolApi.Domain.Entities;
+
+namespace SchoolApi.Controllers
+{
+    [ApiController]
+    [Route("Api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentDBService _studentDBService;
+        public StudentController(StudentDBService studentDBService) 
+        {
+            _studentDBService = studentDBService;
+        }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<Student>> GetAsync()
+        {
+            return await _studentDBService.ReadAsync();
+        }
+    }
+}
